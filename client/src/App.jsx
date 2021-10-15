ed213466fc29a814b4012121ceaf6472ddb6a86e
Sattwik <sahusattwik@gmail.com>
2021-10-15 21:02:03 +0530

    Ready to deploy - Got the villains list in - Added tailwind css styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,24 @@ import axios from "axios";
 
 const App = (props) => {
   const [heros, setHeros] = React.useState([]);
+  const [villains, setVillains] = React.useState([]);
 
   React.useEffect(() => {
     axios({
       url: "http://localhost:8000/heros/all",
     }).then((res) => setHeros(res.data));
+
+    axios({
+      url: "http://localhost:8000/villains/all",
+    }).then((res) => setVillains(res.data));
   }, []);
 
   return (
-    <div className="App">
-      <CharacterList characters={heros} title="Heros" />
+    <div className="App text-gray-700">
+      <div className="lists flex">
+        <CharacterList characters={heros} title="Heros" />
+        <CharacterList characters={villains} title="Villains" />
+      </div>
     </div>
   );
 };
--- client/src/components/CharacterList.jsx
+++ client/src/components/CharacterList.jsx
@@ -2,20 +2,29 @@ import * as React from "react";
 
 const CharacterList = ({ characters, title }) => {
   return (
-    <div className="CharacterList">
-      <div className="title">{title}</div>
+    <div className="CharacterList flex-1 p-4 mx-4 my-3 border-2 border-indigo-100 rounded-md">
+      <div className="title text-2xl text-indigo-700 font-bold mb-3">
+        {title}
+      </div>
       {characters?.length > 0 ? (
         <ul className="characters">
           {characters.map((character) => (
-            <li className="character">
-              <span className="id">{character.id}</span>
-              <span className="name">{character.name}</span>
-              <span className="arc">{character.arc}</span>
+            <li
+              className="character rounded-md p-1.5 hover:bg-gray-100 transition duration-200"
+              key={character.id}
+            >
+              <span className="id mr-2 text-pink-400 font-extrabold">
+                #{character.id}
+              </span>
+              <span className="name text-lg mr-1">{character.name}</span>
+              <span className="arc text-indigo-700">({character.arc})</span>
             </li>
           ))}
         </ul>
       ) : (
-        <div className="empty_message">Loading list `{title}`</div>
+        <div className="empty_message text-3xl text-gray-300">
+          Loading list `{title}`
+        </div>
       )}
     </div>
   );
